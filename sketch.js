95fda651af26507353c8b4bb6b958ab2cc218763
Ben Leung <benjamin.leung80@gmail.com>
2018-10-29 15:45:19 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+// declare bgm variable
+var bgm;
+
+function preload()
+{
+	// load sound file into variable bgm
+	bgm = loadSound("https://bleungwpg.github.io/p5jsTutorial4.1/sounds/bensound-creativeminds.mp3");
+
+}
+
+function setup()
+{
+	createCanvas(800,600);
+
+	// play music during setup so that it doesn't loop infinitely
+	bgm.play();
+}
+
+function draw()
+{
+	textSize(12);
+
+	background(125,125,125)
+
+	text("Credit for bgm goes to Benjamin TISSOT from www.bensound.com",10,40);
+	text("Credit for sound effect goes to zapSplat from www.zapsplat.com",10,80);
+
+
+}
+
